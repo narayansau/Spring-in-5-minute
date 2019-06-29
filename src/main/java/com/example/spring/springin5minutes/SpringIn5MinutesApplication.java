db64b7e6d1ab005e92db9706fa92bdb84e55ef45
Narayan Sau <narayansau@yahoo.com>
2019-06-29 08:49:44 +0530

    initial commit on 29th june

---
+++ src/main/java/com/example/spring/springin5minutes/SpringIn5MinutesApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.springin5minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn5MinutesApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run( SpringIn5MinutesApplication.class, args );
+    }
+
+}
